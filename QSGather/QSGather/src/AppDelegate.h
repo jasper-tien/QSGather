637b6b509a4ce8073ab3d7717aa8a8f9d4a16bb7
tianmaotao <tianmaotao@bilibili.com>
2021-11-03 17:36:53 +0800

    init

---
+++ QSGather/QSGather/src/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  QSGather
+//
+//  Created by tianmaotao on 2021/11/3.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ QSGather/QSGather/src/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  QSGather
+//
+//  Created by tianmaotao on 2021/11/3.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
